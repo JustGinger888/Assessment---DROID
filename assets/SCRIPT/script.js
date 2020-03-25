c245b34f5fdfe7bf2c06695ab0e7eb91da68cc2b
JustGinger888 <ethanmoffatt888@icloud.com>
2020-03-25 22:08:25 +0000

    policies

--- assets/SCRIPT/script.js
+++ assets/SCRIPT/script.js
@@ -67,3 +67,18 @@ function passwordRepeatValidation(password, passwordRepeat) {
     return true;
 }
 //Form Validation
+
+//Modal Close
+var modal = document.getElementById('id01');
+var modal2 = document.getElementById('id02');
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+  else if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
+
+//Modal Close
